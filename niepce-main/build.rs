7b9ec30b44813e49932913ef008a7652041f3ca3
Hubert Figuière <hub@figuiere.net>
2024-05-04 12:27:04 -0400

    build: remove NIEPCE_LIB_PATH
    Also remove the pkg-config in npc-craw
    
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- niepce-main/build.rs
+++ niepce-main/build.rs
@@ -2,11 +2,4 @@ fn main() {
     if let Ok(asan) = std::env::var("ASAN_LIBS") {
         println!("cargo:rustc-link-lib={asan}");
     }
-
-    // This is expected to be set by the mesonb.build.
-    if let Ok(libpath) = std::env::var("NIEPCE_LIB_PATH") {
-        libpath.split(':').for_each(|s| {
-            println!("cargo:rustc-link-search={s}");
-        });
-    }
 }
